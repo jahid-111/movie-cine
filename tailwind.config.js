d953342bf708e087e8c7cfe3181dedf5617c6d09
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-05-18 21:30:52 +0300

    [0]-installed_react-tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      container: true,
+      padding: "1.25em",
+    },
+    colors: "#00D991",
+  },
+  plugins: [],
+};
